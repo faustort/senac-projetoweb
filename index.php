463913c1a58fca7218b33d6608e6b5c6c0112122
AngeloAugustoCSW <angelo.weimar@hotmail.com>
2022-09-16 21:59:51 -0300

    Added SCSS + updates

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include "header.php";
     <div class="row">
         <div class="col-6"><img src="assets/images/download.jpg" alt="random image"></div>
         <div class="col-6">
+            <a href="//google.com" target="_blank" class="btn">Visita o Google 👍🗿</a>
             conteudo 🗿
         </div>
     </div>
