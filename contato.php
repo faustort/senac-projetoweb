89333fe4bc11c6ec2b5231f4b8116743ec8d9505
Fausto <faustortoloi@gmail.com>
2022-09-22 19:57:17 -0300

    contato inicial

---
+++ contato.php
@@ -0,0 +1,26 @@
+<?php
+include "header.php";
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Fale conosco</h1>
+            <div class="">
+                <form action="">
+                    <div>
+                        <label for="nome">Digite seu nome</label>
+                        <input type="text" name="nome" id="nome">
+                    </div>
+                    <div>
+                        <label for="email">Digite seu e-mail</label>
+                        <input type="text" name="email" id="email">
+                    </div>
+                    <input type="submit" value="Enviar">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+include "footer.php";
+?>
